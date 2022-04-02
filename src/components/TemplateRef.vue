9af3e4a6276eb0c6e1e4030e3db5a46c316f461c
Alex_Wang <k8cchaha@gmail.com>
2022-04-02 19:27:17 +0800

    do directive

--- src/components/TemplateRef.vue
+++ src/components/TemplateRef.vue
@@ -1,18 +1,20 @@
 <template>
-  <input ref="aabb" type="text" placeholder="Plz input" />
+  <input v-focus type="text" placeholder="Plz input" v-model="num" />
+  <h1 v-price="num"></h1>
 </template>
 
 <script>
 import { ref } from "@vue/reactivity";
-import { onMounted } from "@vue/runtime-core";
+// import { onMounted } from "@vue/runtime-core";
 export default {
   setup() {
     const aabb = ref(null);
-    onMounted(() => {
-      aabb.value.focus();
-    });
+    const num = ref(123123);
+    // onMounted(() => {
+    //   aabb.value.focus();
+    // });
 
-    return { aabb };
+    return { aabb, num };
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { numPrice } from "./lib/tools";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.directive("focus", {
+  mounted(el) {
+    el.focus();
+  },
+});
+
+app.directive("price", {
+  mounted(el, binding) {
+    const p = numPrice(binding.value);
+    el.innerHTML = p;
+  },
+  updated(el, binding) {
+    const p = numPrice(binding.value);
+    el.innerHTML = p;
+  },
+});
+
+app.mount("#app");
